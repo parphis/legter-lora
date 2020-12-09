707641f5357e72f48e9a985687191f7c92a550c3
Istvan Vig <istvan.vig@parphis.hu>
2020-12-09 23:00:32 +0100

    from server

--- main-server.py
+++ main-server.py
@@ -14,16 +14,17 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
 
 # create a raw LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
 i = 0
 while True:
     # send some data
-    s.setblocking(True)
-    s.send('Hello from parphis-laptop')
+    #s.setblocking(True)
+    #s.send('Hello from parphis-laptop')
 
     # get any data received...
-    s.setblocking(False)
+    #s.setblocking(False)
     data = s.recv(64)
     print('{1}. received: {0}'.format(data, i))
     i = i + 1
     # wait a random amount of time
-    time.sleep(machine.rng() & 0x0F)
+    time.sleep(1) #machine.rng() & 0x0F)
